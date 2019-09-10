6c578aa3691da74deb02b7a66bdc40c19ba6efab
rlawodn6548 <rlawodn6548@naver.com>
2019-09-10 17:34:27 +0900

    delete

--- htdocs/htdocs/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['login_id']);
-header('Location: ./login_screen.html');
-
-?>
--- htdocs/htdocs/upload.php
+++
@@ -1,9 +0,0 @@
-﻿<head>
-    <meta charset="UTF-8">
-	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
-    <title>CREATE</title>
-</head>
-<form enctype='multipart/form-data' action='upload_ok.php' method='post'>
-	<input type='file' name='myfile'>
-	<button>업로드</button>
-</form>
